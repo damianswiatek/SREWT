c70d51f74deab98bb9715a29c3607a04935ee16b
Damian Świątek <damian.topdevs@outlook.com>
2018-12-01 13:30:22 +0100

    init

---
+++ DataModel/Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace DataModel.Repository.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        System.Data.Entity.DbContext Context { get; }
+        IRepository<T> Repository<T>() where T : class;
+        Task<bool> SaveAsync();
+        bool Save();
+    }
+}
