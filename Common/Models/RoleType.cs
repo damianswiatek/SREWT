80e01de7b97933d77f083aeffd89c9defcf8bc94
Damian Świątek <damian.topdevs@outlook.com>
2019-01-08 12:41:03 +0100

    Add seed and mapers to database container

--- Common/Models/RoleType.cs
+++ Common/Models/RoleType.cs
@@ -8,6 +8,7 @@ namespace Common.Models
 {
     public class RoleType
     {
+        public const string Admin = "Admin";
         public const string Employer = "Employer";
         public const string HumanResources = "Human resources";
         public const string Worker = "Worker";
--- DataModel/Container/DatabaseContainer.cs
+++ DataModel/Container/DatabaseContainer.cs
@@ -7,7 +7,10 @@ namespace DataModel.Container
     public class DatabaseContainer : DbContext
     {
         public DbSet<User> Users { get; set; }
-        
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Conclusion> Conclusions { get; set; }
+
         public DatabaseContainer() : base("name=myDB")
         {
             this.Configuration.EnsureTransactionsForFunctionsAndCommands = true;
@@ -16,6 +19,9 @@ namespace DataModel.Container
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Configurations.Add(new UserMap());
+            modelBuilder.Configurations.Add(new AddressMap());
+            modelBuilder.Configurations.Add(new CompanyMap());
+            modelBuilder.Configurations.Add(new ConclusionMap());
         }
 
         public void MigrateDb()
